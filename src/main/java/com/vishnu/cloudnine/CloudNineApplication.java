963c94f44b54ed72ca9c98fdecc8e657ad54d559
Vishnu Simha <vishnusimha01@gmail.com>
2023-04-01 15:24:09 +0100

    for war

--- src/main/java/com/vishnu/cloudnine/CloudNineApplication.java
+++ src/main/java/com/vishnu/cloudnine/CloudNineApplication.java
@@ -3,10 +3,11 @@ package com.vishnu.cloudnine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableConfigurationProperties()
 @SpringBootApplication
-public class CloudNineApplication {
+public class CloudNineApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudNineApplication.class, args);
