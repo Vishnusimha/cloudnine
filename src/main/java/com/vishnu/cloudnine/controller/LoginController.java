9815f6a5c7bbf201a4f454413f92d93ea8d7c38b
Vishnu Simha <vishnusimha01@gmail.com>
2023-04-12 22:19:08 +0100

    clean up

--- src/main/java/com/vishnu/cloudnine/controller/LoginController.java
+++ src/main/java/com/vishnu/cloudnine/controller/LoginController.java
@@ -9,34 +9,6 @@ import java.util.*;
 @RestController
 @RequestMapping("/auth")
 public class LoginController {
-
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<Object> login(@RequestBody Map<String, String> credentials) {
-//        System.out.println("username  password");
-//        String username = credentials.get("username");
-//        String password = credentials.get("password");
-//        System.out.println(username + "  " + password);
-//        // Perform authentication logic here...
-//        Map<String, Object> response = new HashMap<>();
-//
-//        if (Objects.equals(username, "vishnu") && Objects.equals(password, "v")) {
-//            // If authentication succeeds, return a success response with the redirect URL
-//            response.put("success", true);
-//            response.put("redirectUrl", "http://localhost:8081/CloudnineBallloonDecor/eventsdata.html");
-//            return ResponseEntity.ok().body(response);
-//        } else if (username != null && password != null) {
-//            // If authentication succeeds, return a success response with the redirect URL
-//            response.put("admin", true);
-//            response.put("redirectUrl", "/http://localhost:8081/CloudnineBallloonDecor/index.html");
-//            return ResponseEntity.ok().body(response);
-//        } else {
-//            response.put("Auth failed", true);
-//            response.put("redirectUrl", "/http://localhost:8081/CloudnineBallloonDecor/login.html");
-//            return ResponseEntity.badRequest().body(response);
-//        }
-//    }
-
     @PostMapping("/login")
     @CrossOrigin(origins = "*")
     public ResponseEntity<Object> login(@RequestBody Map<String, String> credentials) {
@@ -57,7 +29,6 @@ public class LoginController {
                 break;
             }
         }
-
         if (isAdmin) {
             // If authentication succeeds for an admin, return a success response with the redirect URL for the admin page
             response.put("success", true);
